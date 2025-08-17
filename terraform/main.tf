d8d51f4e5522b267195b9aff46f60a175735e8a1
alozeus1 <100092234+alozeus1@users.noreply.github.com>
2025-08-17 00:57:58 -0600

    Make security group optional

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "appstream_sg" {
+  count       = var.security_group_id == null ? 1 : 0
   name        = "appstream-sg"
   description = "Security group for AppStream fleet"
   vpc_id      = var.vpc_id
@@ -39,16 +40,20 @@ resource "aws_security_group" "appstream_sg" {
   }
 }
 
+locals {
+  effective_sg_id = var.security_group_id != null ? var.security_group_id : aws_security_group.appstream_sg[0].id
+}
+
 resource "aws_cloudformation_stack" "appstream_stack" {
   name          = var.stack_name
   template_body = file("${path.module}/../cft/appstream-stack.yaml")
 
   parameters = {
-    VPCId           = var.vpc_id
-    SubnetIds       = join(",", var.subnet_ids)
-    SecurityGroupId = aws_security_group.appstream_sg.id
-    FleetName       = var.fleet_name
-    SessionTimeout  = var.session_timeout
+    VPCId             = var.vpc_id
+    SubnetIds         = join(",", var.subnet_ids)
+    SecurityGroupId   = local.effective_sg_id
+    FleetName         = var.fleet_name
+    SessionTimeout    = var.session_timeout
     EnableAutoScaling = var.enable_autoscaling
     DesiredCapacity   = var.desired_capacity
     MinCapacity       = var.min_capacity
